624f5a7ddbb746e3755bc02c5dfc42b7d4585e38
Markus Zeller <git@markuszeller.com>
2024-02-10 15:08:55 +0100

    First version

---
+++ src/com.markuszeller.smartdsl.sdPlugin/actions/dsl-speed/property-inspector/inspector.js
@@ -0,0 +1,25 @@
+$PI.onConnected((jsn) => {
+    const form = document.querySelector('#property-inspector');
+    const {actionInfo, appInfo, connection, messageType, port, uuid} = jsn;
+    const {payload, context} = actionInfo;
+    const {settings} = payload;
+
+    Utils.setFormValue(settings, form);
+
+    form.addEventListener(
+        'input',
+        Utils.debounce(150, () => {
+            const value = Utils.getFormValue(form);
+            $PI.setSettings(value);
+        })
+    );
+
+    console.log("Property Inspector connected");
+});
+
+$PI.onSendToPropertyInspector(action, (payload) => {
+   console.log(payload);
+   document.getElementById('unit_radio1').style.border = '1px solid white';
+});
+
+document.getElementById('xxx').style.border = '1px solid white';
