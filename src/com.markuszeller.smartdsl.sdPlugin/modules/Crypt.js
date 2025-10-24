f6fc3987605c5149522d50791d48f7aff3f5368c
Markus Zeller <git@markuszeller.com>
2025-10-24 11:16:11 +0200

    Improve error handling

--- src/com.markuszeller.smartdsl.sdPlugin/modules/Crypt.js
+++ src/com.markuszeller.smartdsl.sdPlugin/modules/Crypt.js
@@ -26,17 +26,23 @@ export class Crypt {
             this.history           = text;
         }
 
-        const decryptText = sjcl.decrypt(
-            this.keyArray,
-            `{"iv":"${this.iv}","v":1,"iter":1000,"ks":256,"ts":128,"mode":"ccm","adata":"","cipher":"aes","salt":"","ct":"${sjcl.codec.base64.fromBits(sjcl.codec.hex.toBits(text))}"}`
-        );
+        try {
+            const iv         = sjcl.codec.base64.fromBits(sjcl.codec.hex.toBits(this.key.slice(0, 16)));
+            const cypherText = sjcl.codec.base64.fromBits(sjcl.codec.hex.toBits(text));
+            const decrypted  = sjcl.decrypt(
+                sjcl.codec.hex.toBits(this.key),
+                '{"iv":"' + iv + '","v":1,"iter":1000,"ks":256,"ts":128,"mode":"ccm","adata":"","cipher":"aes","salt":"","ct":"' + cypherText + '"}'
+            );
 
-        JSON.parse(decryptText).map(element => {
-            const key = element.varid;
-            if (key.startsWith('dsl_') || key.startsWith('inet_')) {
-                this.values[key] = element.varvalue;
-            }
-        });
+            JSON.parse(decrypted).map(element => {
+                const key = element.varid;
+                if (key.startsWith('dsl_') || key.startsWith('inet_')) {
+                    this.values[key] = element.varvalue;
+                }
+            });
+
+        } catch (exception) {
+        }
 
         return this.values;
     }
