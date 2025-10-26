802b342e97c75ab501fd98d86b6aa770248a80ea
Markus Zeller <git@markuszeller.com>
2025-10-26 15:26:09 +0100

    Extract variables

--- build.php
+++ build.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 const PLUGIN   = 'com.markuszeller.smartdsl.sdPlugin';
+const STREAMDECK_EXTENSION = '.streamDeckPlugin';
 const MANIFEST = 'manifest.json';
 
 chdir('src');
@@ -11,4 +12,4 @@ $json = json_decode(file_get_contents(PLUGIN . DIRECTORY_SEPARATOR . MANIFEST), 
 $json->Version = sprintf('%d.%d.%d', $major, $minor, ++$build);
 file_put_contents(PLUGIN . DIRECTORY_SEPARATOR . MANIFEST, json_encode($json, JSON_PRETTY_PRINT));
 
-system('zip -r ../' . PLUGIN . '.streamDeckPlugin ' . PLUGIN . '/*');
+system('zip -r ../' . PLUGIN . STREAMDECK_EXTENSION . ' ' . PLUGIN . '/*');
